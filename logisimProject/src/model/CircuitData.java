ada7d14ee03eb9d156b520a98e6bd3e546942317
KarlSchnalzer <32878278+KarlSchnalzer@users.noreply.github.com>
2019-02-07 11:14:00 -0500

    added comments

--- logisimProject/src/model/CircuitData.java
+++ logisimProject/src/model/CircuitData.java
@@ -1,7 +1,12 @@
 package model;
 
 import java.util.HashMap;
-
+/**
+ * 
+ * @author Karl Schnalzer
+ * Class CircuitData is a java class which keeps track of all circuit 
+ * data(connections, x/y-coordinates, etc.)
+ */
 public class CircuitData {
 	private HashMap<Integer,LogicGate> logicGates;
 	private int id;
@@ -10,7 +15,10 @@ public class CircuitData {
 		logicGates = new HashMap<Integer, LogicGate>();
 		id = 1;
 	}
-	
+	/**
+	 * Adds the type of logic gate to the "Gates map"
+	 * @param lg - the logic gate being added to the map
+	 */
 	public void addGate(LogicGate lg){
 		logicGates.put(id,lg);
 		id++;
